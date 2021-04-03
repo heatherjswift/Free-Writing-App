eb2b15e9d8c87703f7b29d365bd9c417763184ae
Hede <Heatherj.swift@gmail.com>
2021-04-02 20:04:35 -0400

    fix next prompt button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var promptArray = [
     "A houseplant is dying. Tell it why it needs to live.",
     "Write a social media status update for New Years 50 years from now.",
     "You're an astronaut. Describe your perfect day.",
-    "A man giving a speech to a crowd of thousands is suddenly caught in a bald-faced lie. What does he do?",
+    "How a cat sees the world",
     "You're a fortune teller. Tell your favorite protagonist or villian their future."
 ];
 
