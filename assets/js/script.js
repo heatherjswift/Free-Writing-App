60155d340597f020960c86054abc1ce5638789ed
Hede <Heatherj.swift@gmail.com>
2021-04-08 21:46:25 -0400

    timer has a start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,7 +132,13 @@ curatedPhotos(pagenr);
 
 //timer(s)
 
-function startTimer(duration, display) {
+function startTimer() {
+    var fiveMinute = 60 * 5,
+    display = document.querySelector('#time');
+    beginTime(fiveMinute, display);
+}
+
+function beginTime(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
@@ -148,11 +154,8 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-
-function startTimer() {
-    var fiveMinute = 60 * 5,
-    display = document.querySelector('#time');
-    startTimer(fiveMinute, display);
-}
+const startButton = document.getElementById('5-min-btn')
 
 
+
+startButton.addEventListener('click', startTimer)
